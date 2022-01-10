1bff103a23fe50a0c2c1511b4c4c218429da970f
ToodlyDave <alapatdavid@gmail.com>
2022-01-10 08:30:13 +0530

    Welcome message

---
+++ src/main/java/com/addressbook/entities/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.addressbook.entities;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome to the Address Book Program");
+	}
+}
