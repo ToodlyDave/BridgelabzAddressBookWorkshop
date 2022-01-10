6ce5ae7e579c757f1d232f9b33a61e1a2663dad8
ToodlyDave <alapatdavid@gmail.com>
2022-01-10 08:35:53 +0530

    CREATE contact class

---
+++ src/main/java/com/addressbook/entities/Contact.java
@@ -0,0 +1,14 @@
+package com.addressbook.entities;
+
+public class Contact {
+
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	int phone;
+	String email;
+
+}
--- src/main/java/com/addressbook/services/AddressBookMain.java
+++ src/main/java/com/addressbook/services/AddressBookMain.java
@@ -1,8 +1,9 @@
-package com.addressbook.entities;
+package com.addressbook.services;
 
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome to the Address Book Program");
+
 	}
 }
